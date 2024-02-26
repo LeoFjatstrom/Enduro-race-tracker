88d29c520f0f34777290291bf6790f8e94be71bc
LeoFjatstrom <leofjatstrom@gmail.com>
2024-02-26 20:52:14 +0100

    root home route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :competitions
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: "home#index"
+
+  resources :competitions
 end
