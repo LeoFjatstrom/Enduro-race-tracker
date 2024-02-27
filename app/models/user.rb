d3eb1a00e811f520be46a72c767016083e2ecb38
LeoFjatstrom <leofjatstrom@gmail.com>
2024-02-27 12:36:41 +0100

    one more fix in last commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :registrations
+  has_many :station_registrations
 end
