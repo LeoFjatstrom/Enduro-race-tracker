2a2a94ace41a812126954142211081338341daee
LeoFjatstrom <leofjatstrom@gmail.com>
2024-03-01 00:34:52 +0100

    some improvements to registering drivers at a station

--- app/controllers/user/station_registrations_controller.rb
+++ app/controllers/user/station_registrations_controller.rb
@@ -4,45 +4,44 @@ class User::StationRegistrationsController < ApplicationController
 
   def new
     @station_registration = StationRegistration.new
-    @competition = Competition.find(params[:competition_id])
-    @stations = @competition.stations
+    @stations = @competition.stations.order(:name)
+    @previous_registrations = StationRegistration.where(competition_id: @competition.id).order(passed_at: :desc)
 
-    # Set the initially selected station if one is stored in the session
-    if session[:station_id]
-      @selected_station_id = session[:station_id]
+    # Set the initially selected station if one is stored in the session or passed as a parameter
+    @selected_station_id = session[:station_id] || params[:station_id]
+
+    if @selected_station_id.present?
+      @current_station = Station.find(@selected_station_id)
+      @previous_registrations_for_current_station = @current_station.station_registrations.order(passed_at: :desc)
     end
-  end  
+  end
 
   def create
     station_id = params[:station_registration][:station_id]
     start_number = params[:station_registration][:start_number]
     competition_id = params[:competition_id]
   
-    # Attempt to find the competition entry using the provided start_number and competition_id
     competition_entry = CompetitionEntry.find_by(start_number: start_number, competition_id: competition_id)
   
-    # If a competition entry is found, proceed with creating the station registration
     if competition_entry
       @station_registration = StationRegistration.new(
         user: current_user,
         station_id: station_id,
-        driver_id: competition_entry.driver_id, # Use driver_id directly
-        competition_id: competition_id
+        driver_id: competition_entry.driver_id, # Link to the driver through the competition entry
+        competition_id: competition_id,
+        passed_at: Time.current
       )
   
       if @station_registration.save
-        session[:station_id] = @station_registration.station_id
-        redirect_to new_user_competition_station_registration_path(competition_id), notice: 'Successfully registered driver passing through the station.'
+        session[:station_id] = station_id
+        redirect_to new_user_competition_station_registration_path(competition_id, station_id: station_id), notice: 'Successfully registered driver passing through the station.'
       else
-        # Handle the case where station registration couldn't be saved
         render :new, alert: 'Could not register the driver. Please check the details and try again.'
       end
     else
-      # Handle the case where no matching competition entry was found
       redirect_to new_user_competition_station_registration_path(competition_id), alert: 'No matching driver found for the provided start number in this competition.'
     end
   end
-  
 
   def edit
   end
@@ -68,10 +67,4 @@ class User::StationRegistrationsController < ApplicationController
     def station_registration_params
       params.require(:station_registration).permit(:station_id, :start_number, :passed_at)
     end
-
-    def clear_station
-      session.delete(:station_id)
-      redirect_to some_path, notice: 'Station selection cleared.'
-    end
-
 end
