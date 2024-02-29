da45748f8620d470df8ec3a33174a5778e069c60
LeoFjatstrom <leofjatstrom@gmail.com>
2024-02-29 12:39:19 +0100

    added start numberrs to comp entries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_27_113319) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_27_212045) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_27_113319) do
     t.integer "competition_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "start_number"
+    t.index ["competition_id", "start_number"], name: "index_competition_entries_on_competition_id_and_start_number", unique: true
     t.index ["competition_id"], name: "index_competition_entries_on_competition_id"
     t.index ["driver_id"], name: "index_competition_entries_on_driver_id"
   end
